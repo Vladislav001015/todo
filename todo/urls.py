58d12ed1fad948b863c8c932980ac7a1501776ff
vladislav001015 <vladislav001015@gmail.com>
2022-07-13 10:08:10 +0600

    new project

---
+++ todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from todo.views import *
+
+urlpatterns = [
+    path('', TaskListView.as_view()),
+    path('create/', TaskCreateView.as_view()),
+    path('detail/<int:id>/', TaskDetailView.as_view()),
+    path('update/<int:pk>/', TaskUpdateView.as_view()),
+    path('delete/<int:pk>/', TaskDeleteView.as_view())
+
+]
